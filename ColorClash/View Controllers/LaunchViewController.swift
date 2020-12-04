a025665b0e98dc1a9d038cb8e97e95e62deb530e
John Lollar <parking.pal.main@gmail.com>
2020-12-03 21:31:20 -0600

    Added Launch VC functionality and added in Menu VC

--- ColorClash/View Controllers/LaunchViewController.swift
+++ ColorClash/View Controllers/LaunchViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class LaunchViewController: UIViewController {
 
+    @IBOutlet weak var tmpLogo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,7 +20,15 @@ class LaunchViewController: UIViewController {
     }
     
     func animate() {
-        
+        UIView.animate(withDuration: 0.5) {
+            self.tmpLogo.transform = CGAffineTransform(scaleX: 10.0, y: 10.0)
+            self.tmpLogo.alpha = 0.0
+        } completion: { (done) in
+            if done {
+                //will use this probably similar to Trivia Ace where it will check to see if they are a new user or not. For now, will just continue to VC
+            }
+        }
+
     }
 
 }
