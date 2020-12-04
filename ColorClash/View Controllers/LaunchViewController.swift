998cebb603fec99e44b1ecbb84e1ab94f223a167
John Lollar <parking.pal.main@gmail.com>
2020-12-03 21:22:28 -0600

    Added Launch View Controller

---
+++ ColorClash/View Controllers/LaunchViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LaunchViewController.swift
+//  ColorClash
+//
+//  Created by ParkingPal on 12/3/20.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    
+    func animate() {
+        
+    }
+
+}
