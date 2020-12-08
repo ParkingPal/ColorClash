c2005dbb162cb04352ad59f447af367c890f440b
John Lollar <parking.pal.main@gmail.com>
2020-12-08 01:49:30 -0600

    Added new VCs to match Tabs

---
+++ ColorClash/View Controllers/Profile_ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Profile_ViewController.swift
+//  ColorClash
+//
+//  Created by ParkingPal on 12/8/20.
+//
+
+import UIKit
+
+class Profile_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Josefin Sans", size: 30.0)!, NSAttributedString.Key.foregroundColor: UIColor(red: 176/255, green: 224/255, blue: 230/255, alpha: 1.0)]
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
